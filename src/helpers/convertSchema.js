f9143a7c4a164d793d1354fa9348d38c71619234
Alesi-Andreya Ladas <alesiladas@gmail.com>
2018-04-13 16:02:42 -0400

    helper function convertSchema

---
+++ src/helpers/convertSchema.js
@@ -0,0 +1,19 @@
+//function that converts schema too object representation
+const Sequelize = require('sequelize');
+
+const convertSchemas = (schema) => {
+    let schemas = {};
+    for (let modelKey in schema.models) {
+        const model = schema.models[modelKey];
+        let columnNames = [];
+        for (let attribute in model.rawAttributes) {
+            columnNames.push(attribute);
+        }
+
+        schemas[modelKey] = columnNames;
+    }
+    
+    return schemas;
+}
+
+module.exports = convertSchemas;
